3e78f84bfcc645ca89997e6aa9eeed5dcb664abf
Your Name <you@example.com>
2015-11-01 01:50:29 +0000

    query() now returns the correct key and value for each item.

--- lib/myuniversaljobsmatch.rb
+++ lib/myuniversaljobsmatch.rb
@@ -64,17 +64,14 @@ class MyUniversalJobsMatch
   def query(id)
     
     url = @url_base + 'GetJob.aspx?JobID=' + id
-    doc = Nokorexi.new(url).to_doc
+    buffer = RXFHelper.read(url).first
+    doc = Nokorexi.new(buffer.gsub(/<br\s*\/?>/i,'')).to_doc
     content = doc.root.at_css '.jobViewContent'
     title = content.element('h2[2]/text()')
     description = content.element('div')
 
-    raw_summary = doc.root.at_css('.jobViewSummary').xpath('dl/dd/text()')
-    job_id, posting_date, company, location, industries, job_type = raw_summary
-    
-    fields = %i(job_id posting_date company location industries job_type 
-                                                             title description)
-    Hash[fields.zip(raw_summary + [title, description])]
+    a = doc.root.at_css('.jobViewSummary').xpath('dl/*/text()')
+    Hash[a.each_slice(2).map{|k,v| [k.downcase.gsub(' ','_').to_sym,v]}]
     
   end
 end
